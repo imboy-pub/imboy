0fa6e2d977dc7bd353ca2e5f3aaf533f870090a2
leeyi <leeyisoft@qq.com>
2024-09-20 15:50:38 +0800

    定时每日整库备份 pg16

--- doc/postgresql/cron_backup_pgsql.sh
+++ doc/postgresql/cron_backup_pgsql.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# cp -rf /www/wwwroot/imboy-api/doc/postgresql/cron_backup_pgsql.sh /data/docker/imboy_pg15/ && chmod +x /data/docker/imboy_pg15/cron_backup_pgsql.sh
+# cp -rf /www/wwwroot/imboy-api/doc/postgresql/cron_backup_pgsql.sh /data/docker/imboy_pg16/ && chmod +x /data/docker/imboy_pg16/cron_backup_pgsql.sh
 
-# docker exec -it imboy_pg15 /bin/sh
+# docker exec -it imboy_pg16 /bin/sh
+# alias crontab='VIEDIT="vim" crontab -e'
 # crontab -e 新增下面一行
-# 0 3 * * * /usr/bin/docker exec imboy_pg15 /bin/sh /var/lib/postgresql/data/cron_backup_pgsql.sh
-# /usr/bin/docker exec imboy_pg15 pg_dump -h 127.0.0.1 -p 5432 -U imboy_user -d imboy_v1 -s -f /var/lib/postgresql/data/back_pgsql/imboy_v1_dev.sql
+# 0 3 * * * /usr/bin/docker exec imboy_pg16 /bin/sh /var/lib/postgresql/data/cron_backup_pgsql.sh
+# /usr/bin/docker exec imboy_pg16 pg_dump -h 127.0.0.1 -p 5432 -U imboy_user -d imboy_v1 -s -f /var/lib/postgresql/data/back_pgsql/imboy_v1_dev.sql
 
 #
 # 进入 pgsql的docker容器
-# clear && docker container exec -it imboy_pg15 bash
-# /var/lib/postgresql/data/ 等价于 宿主机的 /data/docker/imboy_pg15 目录
+# clear && docker container exec -it imboy_pg16 bash
+# /var/lib/postgresql/data/ 等价于 宿主机的 /data/docker/imboy_pg16 目录
 mkdir -p /var/lib/postgresql/data/back_pgsql && cd /var/lib/postgresql/data/back_pgsql
 # 备份
 pg_dump -h 127.0.0.1 --inserts -d imboy_v1 -U imboy_user -p 5432  -f imboy_v1.sql
